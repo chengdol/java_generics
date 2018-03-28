33f9148cbcee7fec35fdb44bb45f193adee99507
chengdol <chengdol@usc.edu>
2018-03-28 15:25:25 -0700

    generics method and varargs

---
+++ src/generics_method/Lists.java
@@ -0,0 +1,30 @@
+package generics_method;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// convert array to list
+// using generics method and varargs
+public class Lists
+{
+	// varargs always receive an unchecked warning
+	@SafeVarargs
+	public static <T> List<T> toList(T... arr)
+	{
+		List<T> tmp = new ArrayList<>();
+		for (T n : arr) { tmp.add(n); }
+		
+		return tmp;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(toList(new Double[]{3.23, 45.4, -34.4}));
+		// varargs will create array from inputs
+		System.out.println(Lists.toList("1", "2", "3"));
+		// explicitly give the type
+		// sometimes this is necessary
+		System.out.println(Lists.<String>toList("1", "2", "3"));
+		
+	}
+}
