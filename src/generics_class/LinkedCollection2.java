24b27912a247a6426c4eaf28d8519a1ae4e02385
chengdol <chengdol@usc.edu>
2018-04-08 11:22:16 -0700

    generics class with static nested class

--- src/generics_class/LinkedCollection2.java
+++ src/generics_class/LinkedCollection2.java
@@ -1,24 +1,69 @@
 package generics_class;
 
 import java.util.AbstractCollection;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 // it has a static nested class
 public class LinkedCollection2<E> extends AbstractCollection<E>
 {
-
+	private static class Node<T>
+	{
+		private T value;
+		private Node<T> next = null;
+		private Node(T val) { value = val; }
+	}
+	
+	private int size = 0;
+	private Node<E> first = new Node<>(null);
+	private Node<E> last = first;
+	
+	public LinkedCollection2() {}
+	public LinkedCollection2(Collection<? extends E> col) { addAll(col); }
+	
+	@Override
+	public boolean add(E e)
+	{
+		last.next = new Node<E>(e);
+		last = last.next;
+		size++;
+		return true;
+	}
+	
 	@Override
 	public Iterator<E> iterator()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		// because Node is static, 
+		return new Iterator<E>() {
+			private Node<E> current = first;
+			
+			@Override
+			public boolean hasNext()
+			{
+				return current.next != null;
+			}
+
+			@Override
+			public E next()
+			{
+				if (hasNext())
+				{
+					current = current.next;
+					return current.value;
+				}
+				else
+				{
+					throw new NoSuchElementException();
+				}
+			}
+		};
 	}
 
 	@Override
 	public int size()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 }
